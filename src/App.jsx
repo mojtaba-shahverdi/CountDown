237bc36c482943d014bab0ea1f43ba135a6659b0
moji <mojtaba0shahverdi@gmail.com>
2022-10-10 06:26:07 +0330

    final

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './App.scss';
+import Settings from './components/Settings';
+import SettingsContext from './components/SettingsContext';
+import Timer from './components/Timer';
+
+function App() {
+  const [showSettings, setShowSettings] = useState(false)
+  const [workMinutes, setWorkMinutes] = useState(45)
+  const [breakMinutes, setBreakMinutes] = useState(15)
+  return (
+    <main>
+      <SettingsContext.Provider value={{
+        showSettings,
+        setShowSettings,
+        workMinutes,
+        breakMinutes,
+        setWorkMinutes,
+        setBreakMinutes,
+      }}>
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
+    </main>
+  );
+}
+
+export default App;
